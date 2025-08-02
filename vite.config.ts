f7973ff1e0d9650426f56620cc20d03cd1c87161
kiernan-dev <kiernan.devsignr@gmail.com>
2025-08-02 18:22:39 -0500

    update vite config to handle both localhost and gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/tcc-react-forged',
+  base: command === 'serve' ? '/' : '/tcc-react-forged/', // Use '/' for dev, GitHub path for build
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
